931123ddfa914d5c5c281217c08e38b52307be31
UltroA <64162002+UltroA@users.noreply.github.com>
2023-11-21 11:43:33 +0300

    made version with shaders

---
+++ includes.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <SDL.h>
+#ifdef __APPLE__
+    #include <OpenGL/glu.h>
+    #include <OpenGL/glext.h>
+#else
+    #include <GL/glu.h>
+    #include <GL/glext.h>
+    #include <GL/glx.h>
+    #include <GL/glxext.h>
+    #define glXGetProcAddress(x) (*glXGetProcAddressARB)((const GLubyte*)x)
+#endif
+
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
